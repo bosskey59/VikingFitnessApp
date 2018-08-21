fb84ea2adbaa0475d2ec638479c0f8ed3d237dc6
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-21 16:08:04 -0500

    validates and made crud views for exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,6 +2,9 @@ class Exercise < ApplicationRecord
 
 	has_many :viking_exercises
 	has_many :vikings, through: :viking_exercises
+	validates_presence_of :name, :exercise_type, :time, :calories, :instruction_link
+	validates :calories, numericality: { greater_than: 0}
+	validates :time, numericality: { greater_than: 0}
 
-	
+
 end
