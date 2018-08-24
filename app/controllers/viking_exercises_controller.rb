e30a0132c3626430839a5e44710c92dc3c5b8522
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-24 11:10:28 -0500

    UX/UI changes/ easter egg

--- app/controllers/viking_exercises_controller.rb
+++ app/controllers/viking_exercises_controller.rb
@@ -1,5 +1,5 @@
 class VikingExercisesController  < ApplicationController
-
+  before_action :redirect_if_not_logged_in, only: [:create, :destroy]
 
   def create
     VikingExercise.create(viking_id:session[:signed_in_viking_id], exercise_id:session[:current_exercise_id])
@@ -11,7 +11,5 @@ class VikingExercisesController  < ApplicationController
     viking_exercise.destroy
     redirect_to viking_exercise.viking
   end
-
-
 
 end
